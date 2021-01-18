907b5da68357dd5ce1890dd939abc2113a92e5de
austinchen13 <46636123+austinchen13@users.noreply.github.com>
2021-01-17 17:53:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 var express = require("express");
 var path = require('path');
 var app = express();
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/hello.html'))
-
-});
-
-app.get('/HackDavis.JPG', function (req, res) {
-    res.sendFile(path.join(__dirname + '/HackDavis.JPG'))
-
-});
+app.use(express.static(‘/‘))
 app.listen(3000);
 console.log('Express server started');
