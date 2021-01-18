3a163546847034e22f8c9317213f0f87e42bdb31
austinchen13 <46636123+austinchen13@users.noreply.github.com>
2021-01-17 17:34:58 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/index.html'))
+    res.sendFile('index.html')
 });
 app.listen(3000);
