ceee2e84b3245334b3d58124b244eae39fc167b4
austinchen13 <46636123+austinchen13@users.noreply.github.com>
2021-01-17 18:00:47 -0800

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,30 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/hello.html'))
 
 });
-app.use(express.static('/'))
+
+app.get('/HackDavis.JPG', function (req, res) {
+    res.sendFile(path.join(__dirname + '/HackDavis.JPG'))
+
+});
+
+app.get('/Exercise_Club.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Exercise_Club.html'))
+
+});
+
+app.get('/Football_Club.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Football_Club.html'))
+
+});
+
+app.get('/Friends_Club.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Friends_Club.html'))
+
+});
+
+app.get('/Moneyball-Club.html', function (req, res) {
+    res.sendFile(path.join(__dirname + '/Moneyball-Club.html'))
+
+});
 app.listen(3000);
 console.log('Express server started');
