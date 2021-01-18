b02b5a99cd002eb898e83ab805303f4d3ac53696
austinchen13 <46636123+austinchen13@users.noreply.github.com>
2021-01-17 17:56:12 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var path = require('path');
 var app = express();
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/hello.html'))
+
+});
 app.use(express.static('/'))
 app.listen(3000);
 console.log('Express server started');
